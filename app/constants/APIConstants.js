96d27e148f9a08cb8e94e3cac1fc64d02bacddd5
Roy Zhang <chiragjadav134@gmail.com>
2022-08-11 11:48:43 +0530

    Update the Logout feature

--- app/constants/APIConstants.js
+++ app/constants/APIConstants.js
@@ -1,7 +1,9 @@
 
- export const API_BASE_URL= 'http://e805-106-215-33-18.ngrok.io/naxum-test/api/';
+ export const API_BASE_URL= 'http://3efb-106-215-33-18.ngrok.io/naxum-test/api/';
  export const GET_USER='get_user';
  export const LOGIN= 'login';
  export const UPDATE_USER= 'update_user';
  export const ADD_CONTACT= 'add_contact';
  export const SEARCH_CONTACTS= 'get_contacts';
+ 
+ 
--- app/modules/Registration/LogoutScreen.js
+++ app/modules/Registration/LogoutScreen.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import styles from './styles/LogoutScreenStyles';
-import { Button } from '../../components/index';
-import _ from 'lodash';
-import { CommonActions } from '@react-navigation/native';
-import { AppConstants } from '../../constants/index';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import styles from "./styles/LogoutScreenStyles";
+import { Button } from "../../components/index";
+import _ from "lodash";
+import { CommonActions } from "@react-navigation/native";
+import { AppConstants } from "../../constants/index";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 class LogoutScreen extends Component {
-
   constructor(props) {
     super(props);
-  }
-
-  doLogout = async () => {
-    AsyncStorage.setItem(AppConstants.USER_DETAILS, null, () => {
-      this.props.navigation.navigate(AppConstants.LOGIN)
-    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={{ flex: 0.5 }}></View>
-        <Text style={styles.title}>{'Proceed Sign-out?'}</Text>
+        <Text style={styles.title}>{"Proceed Sign-out?"}</Text>
         <View style={styles.actionContainer}>
           <Button
             disabled={false}
-            onPress={this.doLogout()}
-            title={'Yes'}
-            textStyle={{ textAlign: 'center', flex: 1 }}
+            onPress={async () => {
+              await AsyncStorage.removeItem(AppConstants.USER_DETAILS);
+              this.props.navigation.navigate(AppConstants.LOGIN);
+            }}
+            title={"Yes"}
+            textStyle={{ textAlign: "center", flex: 1 }}
             contentStyle={styles.buttonContainer}
           />
           <Button
             disabled={false}
-            onPress={() => this.props.navigation.dispatch(CommonActions.goBack())}
-            title={'Cancel'}
-            textStyle={{ textAlign: 'center', flex: 1 }}
+            onPress={() =>
+              this.props.navigation.dispatch(CommonActions.goBack())
+            }
+            title={"Cancel"}
+            textStyle={{ textAlign: "center", flex: 1 }}
             contentStyle={styles.buttonContainer}
           />
         </View>
@@ -44,6 +42,5 @@ class LogoutScreen extends Component {
     );
   }
 }
-
 
 export default LogoutScreen;
